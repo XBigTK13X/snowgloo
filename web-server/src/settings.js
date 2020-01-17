76a4c88d0d0b2b2dc8167dc9758eed3b57e1c3ff
XBigTK13X <xbigtk13x@gmail.com>
2020-01-17 16:34:41 -0600

    Clean up docker settings

--- web-server/src/settings.js
+++ web-server/src/settings.js
@@ -1,7 +1,11 @@
-module.exports = {
-    webServerPort: 5051,
-    mediaRoot: '/media/trove/media/music',
-    mediaServer: 'http://192.168.1.20:5050',
-    databasePath: '/tmp/snowgloo.json',
-    ignoreDatabaseCache: false,
+const config = {
+    webServerPort: process.env.SNOWGLOO_WEB_API_PORT || 5051,
+    mediaRoot: process.env.SNOWGLOO_MEDIA_ROOT || '/media/trove/media/music',
+    mediaServer: process.env.SNOWGLOO_MEDIA_SERVER_URL || 'http://192.168.1.20:5050',
+    databasePath: process.env.SNOWGLOO_DATABASE_PATH || '/tmp/snowgloo.json',
+    ignoreDatabaseCache: process.env.SNOWGLOO_IGNORE_DATABASE_CACHE || false,
 }
+
+console.log("Configuration read as ",{config})
+
+module.exports = config
