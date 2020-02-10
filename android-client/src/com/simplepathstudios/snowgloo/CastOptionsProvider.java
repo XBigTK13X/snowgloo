406aabeb82c73d7ca1b42f95ec7343e9446ec01a
XBigTK13X <xbigtk13x@gmail.com>
2020-02-10 15:29:08 -0600

    Remove unused cast action buttons

--- android-client/src/com/simplepathstudios/snowgloo/CastOptionsProvider.java
+++ android-client/src/com/simplepathstudios/snowgloo/CastOptionsProvider.java
@@ -21,12 +21,15 @@ import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.NotificationAction;
+import com.google.android.gms.cast.framework.media.NotificationActionsProvider;
 import com.google.android.gms.cast.framework.media.NotificationOptions;
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
 
 import android.content.Context;
 import android.view.Menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CastOptionsProvider implements OptionsProvider {
@@ -34,6 +37,7 @@ public class CastOptionsProvider implements OptionsProvider {
     @Override
     public CastOptions getCastOptions(Context context) {
         NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setActions(null,null)
                 .build();
         CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
                 .setNotificationOptions(notificationOptions)
