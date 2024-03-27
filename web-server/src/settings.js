fa5c92e3be7c5adce07271a64817a667e50e3a9b
XBigTK13X <xbigtk13x@gmail.com>
2024-03-26 20:07:46 -0500

    Correct the default relative media path

--- web-server/src/settings.js
+++ web-server/src/settings.js
@@ -1,7 +1,7 @@
 let config = {
     webServerPort: process.env.SNOWGLOO_WEB_API_PORT || 5051,
     mediaRoot: process.env.SNOWGLOO_MEDIA_ROOT || '/media/trove/media/music',
-    relativeMediaDir: process.env.SNOWGLOO_RELATIVE_MEDIA_DIR || 'trove/media/music',
+    relativeMediaDir: process.env.SNOWGLOO_RELATIVE_MEDIA_DIR || 'trove/media/music/',
     mediaServer: process.env.SNOWGLOO_MEDIA_SERVER_URL || 'http://192.168.1.20:5050',
     databaseDirectory: process.env.SNOWGLOO_DATABASE_DIR || '/home/kretst/snowgloo',
     ignoreDatabaseCache: process.env.SNOWGLOO_IGNORE_DATABASE_CACHE || false,
