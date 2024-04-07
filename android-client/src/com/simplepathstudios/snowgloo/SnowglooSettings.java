24e637e98824bed57a4d848ee73a74acb9c28c11
XBigTK13X <xbigtk13x@gmail.com>
2024-04-07 09:33:13 -0500

    Update dependencies for Pixel 8

--- android-client/src/com/simplepathstudios/snowgloo/SnowglooSettings.java
+++ android-client/src/com/simplepathstudios/snowgloo/SnowglooSettings.java
@@ -4,8 +4,8 @@ package com.simplepathstudios.snowgloo;
 import android.net.Uri;
 
 public class SnowglooSettings {
-    public static final String BuildDate = "March 14, 2024";
-    public static final String ClientVersion = "1.7.0";
+    public static final String BuildDate = "April 07, 2024";
+    public static final String ClientVersion = "1.7.1";
     public static final String DevServerUrl = "http://192.168.1.20:5051";
     public static final String ProdServerUrl = "http://9914.us:5051";
     public static boolean EnableDebugLog = false;
